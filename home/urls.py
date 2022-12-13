792c5e000578d591512a7ffd91a08d92b5a6838b
prabin.shrestha <prabin.shrestha@tradeinsur.com>
2022-12-13 20:39:51 +0545

    index page work going on

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    # path('', views.index,name='index'),
+    path('', views.index,name='index'),
     # path('booking', views.booking,name='booking'),
     # path('my-book', views.myBook,name='mybook'),
     # path('add-book', views.addBook,name='addbook'),
--- home/views.py
+++ home/views.py
@@ -8,8 +8,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import JsonResponse
+from pizza.models import Pizza
 from django.contrib import messages
 import json
 
 # Create your views here.
 
+@login_required(login_url='/login/')
+def index(request):
+    pizzas= Pizza.objects.all()
+    return render(request, 'index.html', {'pizzas': pizzas})
