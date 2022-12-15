23684cbda701843daf2eed33233e38d1e406a8f4
prabin.shrestha <prabin.shrestha@tradeinsur.com>
2022-12-15 09:57:27 +0545

    auth ui ready

--- order/admin.py
+++ order/admin.py
@@ -2,7 +2,7 @@ from order.models import Order
 from django.contrib import admin
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display=('user','pizza','quantity','address','phone_number','total_price')
+    list_display=('user','pizza','quantity','address','phone_number','total_price','created_at')
     search_fields=['address','quantity','phone_number','total_price']
     
 
